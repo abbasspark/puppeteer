d1a71319bb3eb82a395ca437e3f4363732f9d4b1
abbas bandar <abbasbandar@gmail.com>
2024-03-18 16:23:10 +0100

    headless

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ app.get('/screenshot', async (req, res) => {
   const url = req.query.url;
   try {
     // Launch Puppeteer browser
-    const browser = await puppeteer.launch(); // Adjust headless option as needed
+    const browser = await puppeteer.launch({ headless: true }); // Adjust headless option as needed
     const page = await browser.newPage();
-    await page.setViewport({ width: 1920, height: 1080 });
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36');
     // Navigate to the provided URL
     await page.goto(url, { waitUntil: 'networkidle2', timeout: 60000 }); // Wait until there are no more than 2 network connections for at least 500 ms
