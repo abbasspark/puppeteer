3b016b2c6d4bb2a9a1269fc0dda03e35b8917df1
abbas bandar <abbasbandar@gmail.com>
2024-03-19 09:45:58 +0100

    sh

--- start_app.sh
+++ start_app.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
-pkg update -y
-pkg upgrade -y
-pkg install proot-distro openssh python3 -y
-pkg install x11-repo -y
-pkg install tur-repo -y
-pkg install chromium -y
-pkg install nodejs -y
 
-npm start
+# Update package repositories
+pkg update --assume-yes
+
+# Upgrade installed packages
+pkg upgrade --assume-yes
+
+# Install necessary packages
+pkg install git proot-distro openssh python3 x11-repo tur-repo chromium nodejs -y
+
+# Clone the GitHub repository
+git clone https://github.com/abbasspark/puppeteer.git
+
+# Change directory to the cloned repository
+cd puppeteer
+
+# Install pm2 globally
+npm install pm2 -g
+
+# Start the application using pm2
+pm2 start npm --name "puppeteer" -- start
+
+# Save the current pm2 process list
+pm2 save
+
+# Setup pm2 startup to run on system boot
+pm2 startup
