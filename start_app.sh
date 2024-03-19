f83843ba46fc46024c567f86c2227d1f6e3ea4be
abbas bandar <abbasbandar@gmail.com>
2024-03-19 09:13:22 +0100

    new

--- start_app.sh
+++ start_app.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-pkg install root-repo
+pkg update -y
+pkg upgrade -y
+pkg install proot-distro openssh python3 -y
+pkg install x11-repo -y
+pkg install tur-repo -y
+pkg install chromium -y
+pkg install nodejs -y
 
-pkg install docker-compose
-
-docker-compose up -d
+npm start
